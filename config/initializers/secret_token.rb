8638b2577dea2df455aeaac35a7dd29c597c8fec
Max W <xypotion@gmail.com>
2014-05-11 19:56:39 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Coiner::Application.config.secret_key_base = 'dc7dfbb83a4a5b9270b3e24728a01e9502bfffcc019403bede51786951efbe94cf99da3cd3fdf9e24cf9332b7978ac4f47dd6b230f0caba105a5ee3f11ff8684'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Coiner::Application.config.session_store :cookie_store, key: '_coiner_session'
